4c337d92d90b12af4de286b4733555f333789e4d
YsenGrimm <kloeten.wars@googlemail.com>
2014-08-12 23:11:32 +0200

    Not so amazing maze generator.

--- Assets/Scripts/MapGenrator.cs
+++ Assets/Scripts/MapGenrator.cs
@@ -8,59 +8,7 @@ public class MapGenrator : MonoBehaviour {
 	public GameObject walkable;
 	public int worldSize = 33;
 
-	int[,] world;
-	List<Wall> wallList;
-	
-	// Use this for initialization
-	void Start () 
-	{
-		world = new int[worldSize, worldSize];
-		wallList = new List<Wall>();
-
-		int i = 1;
-		int j = 1;
-
-		world[i,j] = 1;
-		addNeighboringCells(i,j);
-
-		while(!(wallList.Count == 0)) {
-
-			killTheOnot(wallList[(int)(Random.value * wallList.Count)]);
-
-		}
-		display();
-	}
-
-	void killTheOnot(Wall w) {
-		int wli = w.i;
-		int wlj = w.j;
-		
-		if (wli > 32) {
-			wli = 32;
-		}
-		if (wlj > 32) {
-			wlj = 32;
-		}
-		if (wli < 0) {
-			wli = 0;
-		}
-		if (wlj < 0) {
-			wlj = 0;
-		}
-		
-		if(world[wli, wlj] == 0) {
-			if (wli >= 1)
-				world[wli-1, wlj] = 1;
-			
-			world[wli, wlj] = 1;
-			addNeighboringCells(wli-1, wlj);
-		} else {
-//			wallList.Remove(wallList[r]);
-		}
-
-	}
-
-	void display() {
+	void display(int[,] world) {
 		for (int i = 0; i < worldSize; ++i) {
 			for (int j = 0; j < worldSize; ++j) {
 				if (world[i,j] == 0) {
@@ -73,88 +21,75 @@ public class MapGenrator : MonoBehaviour {
 			}
 		}
 	}
-
-	void addNeighboringCells(int i, int j) 
-	{
-		//left
-		if (i >= 1 && j >= 1)
-			wallList.Add(new Wall(i-1,j-1));
-		else
-			wallList.Add(new Wall(i,j));
-
-		if (i >= 1)
-			wallList.Add(new Wall(i-1,j));
-		else
-			wallList.Add(new Wall(i,j));
-
-		if (i >= 1)
-			wallList.Add(new Wall(i-1,j+1));
-		else
-			wallList.Add(new Wall(i,j+1));
-
-		//middle
-		if (j >= 1)
-			wallList.Add(new Wall(i,j-1));
-		else
-			wallList.Add(new Wall(i,j));
-
-		wallList.Add(new Wall(i,j+1));
-
-		//right
-		if (j >= 1)
-			wallList.Add(new Wall(i+1,j-1));
-		else
-			wallList.Add(new Wall(i+1,j));
-
-		wallList.Add(new Wall(i+1,j));
-		wallList.Add(new Wall(i+1,j+1));
+	
+	int rand(float x, float y) {
+		return (int)((x + Random.value) * y);
 	}
-
-	void Maze(int width=81, int height=51, float complexity=.75f, float density=.75f) {
-
+	
+	void Maze(int width, int height, float complexity, float density) {
+		
 		//only odd shapes
 		Vector2 shape = new Vector2((height / 2) * 2 + 1, (width / 2) * 2 + 1);
-
+		
 		// Adjust complexity and density relative to maze size
-		complexity = (int) (complexity * (5 * (shape[0] + shape[1])));
-		density    = (int) (density * (shape[0] / 2 * shape[1] / 2));
-
+		complexity = (int)(complexity * (5 * (shape[0] + shape[1])));
+		density    = (int)(density * (shape[0] / 2 * shape[1] / 2));
+		
 		// Build actual maze           
 		int[,] Z;
 		Z = new int[width, height];
-
-		// Fill borders
-		Z[0, 0] = 1;
-		//Z[-1, 0] = 1;
-		Z[0, 0] = 1;
-		//Z[0, -1] = 1;
 		
+		// Fill borders
+		for (int x = 0; x < width; ++x) {
+			Z[0, x] = 1;
+			Z[x, 0] = 1;
+			Z[width-1, x] = 1;
+			Z[x, height-1] = 1;
+		}
+
 		// Make aisles
-		/*
-		for (float i = 0; i < density; ++i) {
-			float x = rand(0, shape[1] / 2) * 2; 
-			float y = rand(0, shape[0] / 2) * 2;
+		for (int i = 0; i < (int)density; ++i) {
+			int x = rand(0, shape[1] / 2) * 2; 
+			int y = rand(0, shape[0] / 2) * 2;
 			Z[y, x] = 1;
-			for (float j = 0; j < complexity; ++j) {
-				int[] neighbours = new int[];
-				if (x > 1) {             neighbours.append((y, x - 2))};
-				if x < shape[1] - 2:  neighbours.append((y, x + 2));
-				if y > 1:             neighbours.append((y - 2, x));
-				if y < shape[0] - 2:  neighbours.append((y + 2, x));
-				if len(neighbours) {
-					y_,x_ = neighbours[rand(0, len(neighbours) - 1)];
-					if Z[y_, x_] == 0 {
+			for (int j = 0; j < (int)complexity; ++j) {
+				List<Vector2> neighbours = new List<Vector2>();
+
+				if (x > 1) { 
+					neighbours.Add(new Vector2(y, x - 2)); 
+				};
+				if (x < shape[1] - 2) { 
+					neighbours.Add(new Vector2(y, x + 2)); 
+				};
+				if (y > 1) { 
+					neighbours.Add(new Vector2(y - 2, x)); 
+				};
+				if (y < shape[0] - 2) { 
+					neighbours.Add(new Vector2(y + 2, x)); 
+				};
+
+				if (neighbours.Count >= 0) {
+					int y_ = (int)neighbours[rand(0, neighbours.Count - 1)].x;
+					int x_ = (int)neighbours[rand(0, neighbours.Count - 1)].y;
+					if (Z[y_, x_] == 0) {
 						Z[y_, x_] = 1;
-						Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1;
-						x, y = x_, y_;
+						Z[y_ + (y - y_) / 2, x_ + (x - x_) / 2] = 1;
+						x = x_;
+						y = y_;
 					}				  
 				}
 			}
 		}
-		*/
+		display(Z);
+	}
+	
+	// Use this for initialization
+	void Start () 
+	{
+		Maze(worldSize, worldSize, .75f, .75f);
 	}
 
-
+	
 	// Update is called once per frame
 	void Update () 
 	{
