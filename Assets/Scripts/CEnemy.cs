aad0e14fc5316bdae70567c51b6c9e12ed585aec
PStang <paul@stacom.de>
2014-08-13 10:41:30 +0200

    Enemy spotting behavior

--- Assets/Scripts/CEnemy.cs
+++ Assets/Scripts/CEnemy.cs
@@ -3,45 +3,67 @@ using System.Collections;
 
 [RequireComponent (typeof(CircleCollider2D))]
 public class CEnemy : MonoBehaviour {
-	
-	public Vector3 m_Pos;
-	public float m_speed=0.5f;
-	public float mScale =0.1f;
-	public bool isseen;
-	Vector3 direction;
-	float lifetime;
-
-
-	void Start () {
-		isseen = false;
-		lifetime = 0;
-		this.transform.localScale = new Vector3 (0.1f,0.1f,0.1f);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		lifetime ++;
-		if (isseen) 
-		{
-			m_speed = 0;
-		}
-		else 
-		{
-			m_speed = 0.1f;
-			if(lifetime%2==0){
-				int i = 1;
-	/*			if(Player.position<10){
-					direction= player.position-pos;
-					this.transfrom.localPosition -=direction;
-				}
-				else*/ if(lifetime%5==0){
-					i=(Random.value * 10 > 5)?-1:1;
-					float rand=Mathf.Sin(Mathf.PI*Random.value);
-					float rand2=Mathf.Cos(Mathf.PI*Random.value);
-					direction = i*m_speed*new Vector3(rand,rand2,0);
-				}
-				this.transform.localPosition += direction;
-			}
-		}
-	}
+    
+    public Vector3 m_Pos;
+    public float m_speed=0.5f;
+    public float mScale =0.1f;
+    public bool isseen;
+    Vector3 direction;
+    float lifetime;
+    public EnemySpotRangeScript spotting;
+    
+    void Start () {
+        isseen = false;
+        lifetime = 0;
+        this.transform.localScale = new Vector3 (0.1f,0.1f,0.1f);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (spotting.spotted)
+            Debug.Log("Player spotted");
+        
+        lifetime ++;
+        if (isseen) 
+        {
+            m_speed = 0;
+        }
+        else 
+        {
+            if(spotting.spotted)
+            {
+                Vector3 p= GameObject.Find("Player").transform.position;
+                direction = (this.transform.localPosition - p).normalized;
+                
+                this.transform.localPosition -=direction.normalized*m_speed;
+                return;
+            }
+            
+            
+            m_speed = 0.1f;
+            if(lifetime%2==0){
+                int i = 1;
+                
+                if(lifetime%5==0)
+                {
+                    i=(Random.value * 10 > 5)?-1:1;
+                    float rand=Mathf.Sin(Mathf.PI*Random.value);
+                    float rand2=Mathf.Cos(Mathf.PI*Random.value);
+                    direction = i*m_speed*new Vector3(rand,rand2,0);
+                }
+                this.transform.localPosition += direction;
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        
+        if (coll.gameObject.tag == "Player")
+        {
+            Player p = coll.gameObject.GetComponent<Player>();
+            GameSingelton.instance.mHealth--;
+        }
+        
+        
+    }
 }
---
+++ Assets/Scripts/EnemySpotRangeScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpotRangeScript : MonoBehaviour
+{
+    
+    public bool spotted = false;
+    float timer;
+    public float spotTime=0.1f;
+    
+    void Update(){
+        
+        timer -= Time.deltaTime;
+        spotted = timer > 0;
+        
+    }
+    
+    void OnTriggerEnter2D (Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+            timer = spotTime;
+    }
+    void OnTriggerStay2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+            timer = spotTime;
+    }
+}
